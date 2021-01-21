0f3b5cff0941c177ba6a3e5c7cda4fe87c8eacba
Elyse Wells <cewells89@gmail.com>
2021-01-20 19:46:47 -0500

    removed extraneous folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css';
+import Donuts from './pages/Donuts';
+
+function App() {
+  return (
+    <div className="App">
+      <Donuts />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Donuts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DonutCard from '../components/DonutCard/DonutCard';
+
+const componentName = () => {
+    return (
+        <div className="container">
+            <DonutCard />
+        </div>
+    );
+};
+
+export default componentName;
